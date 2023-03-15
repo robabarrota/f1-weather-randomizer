f0384549dd16f9eab4cfc94f11945719a58a340a
Rob Abarrota <rob.abarrota@gmail.com>
2023-03-14 23:29:22 -0400

    Make weather more sticky between stints

--- index.js
+++ index.js
@@ -2,32 +2,32 @@
 const trackWetProbabilities = [
     // wetChance formula is wet days in a year / 365 (days in a year) / 3 (divider for stints)
     { name: "Abu Dhabi", wetChance: 9 / 365 / 3},
-    { name: "Austria", wetChance: 146 / 365 / 3},
-    { name: "Australia", wetChance: 36 / 365 / 3},
-    { name: "Azerbaijan", wetChance: 39 / 365 / 3},
-    { name: "Bahrain", wetChance: 13 / 365 / 3},
-    { name: "Belgium", wetChance: 160 / 365 / 3},
-    { name: "Brazil", wetChance: 110 / 365 / 3},
-    { name: "Canada", wetChance: 163 / 365 / 3},
-    { name: "China", wetChance: 123 / 365 / 3},
-    { name: "France", wetChance: 55 / 365 / 3},
-    { name: "Great Britain", wetChance: 137 / 365 / 3},
-    { name: "Hungary", wetChance: 86 / 365 / 3},
-    { name: "Imola", wetChance: 76 / 365 / 3},
-    { name: "Italy", wetChance: 121 / 365 / 3},
-    { name: "Japan", wetChance: 126 / 365 / 3},
-    { name: "Mexico", wetChance: 125 / 365 / 3},
-    { name: "Miami", wetChance: 116 / 365 / 3},
-    { name: "Monaco", wetChance: 86 / 365 / 3},
-    { name: "Netherlands", wetChance: 130 / 365 / 3},
-    { name: "Portugal", wetChance: 49 / 365 / 3},
-    { name: "Saudi Arabia", wetChance: 19 / 365 / 3},
-    { name: "Singapore", wetChance: 167 / 365 / 3},
-    { name: "Spain", wetChance: 80 / 365 / 3},
-    { name: "USA", wetChance: 80 / 365 / 3},
+    { name: "Austria", wetChance: 153 / 365 / 3},
+    { name: "Australia", wetChance: 56 / 365 / 3},
+    { name: "Azerbaijan", wetChance: 60 / 365 / 3},
+    { name: "Bahrain", wetChance: 20 / 365 / 3},
+    { name: "Belgium", wetChance: 167 / 365 / 3},
+    { name: "Brazil", wetChance: 125 / 365 / 3},
+    { name: "Canada", wetChance: 170 / 365 / 3},
+    { name: "China", wetChance: 134 / 365 / 3},
+    { name: "France", wetChance: 79 / 365 / 3},
+    { name: "Great Britain", wetChance: 143 / 365 / 3},
+    { name: "Hungary", wetChance: 107 / 365 / 3},
+    { name: "Imola", wetChance: 105 / 365 / 3},
+    { name: "Italy", wetChance: 133 / 365 / 3},
+    { name: "Japan", wetChance: 134 / 365 / 3},
+    { name: "Mexico", wetChance: 134 / 365 / 3},
+    { name: "Miami", wetChance: 133 / 365 / 3},
+    { name: "Monaco", wetChance: 107 / 365 / 3},
+    { name: "Netherlands", wetChance: 135 / 365 / 3},
+    { name: "Portugal", wetChance: 70 / 365 / 3},
+    { name: "Saudi Arabia", wetChance: 26 / 365 / 3},
+    { name: "Singapore", wetChance: 175 / 365 / 3},
+    { name: "Spain", wetChance: 106 / 365 / 3},
+    { name: "USA", wetChance: 106 / 365 / 3},
 ];
 
-const multiplierWeight = 10;
+const multiplierWeight = 1;
 
 const maxWetChance = Math.max(...trackWetProbabilities.map(({wetChance}) => wetChance));
 const minWetChance = Math.min(...trackWetProbabilities.map(({wetChance}) => wetChance));
@@ -40,9 +40,6 @@ const getWeightedWeatherTypeMultipliers = wetChance => {
     const totalChances = weightedWeatherTypeChances.reduce((partialSum, a) => partialSum + a, 0);
     const weightedWeatherProportions = weightedWeatherTypeChances.map(chance => chance / totalChances);
 
-    const dryWeightedWeatherProportions = weightedWeatherProportions.filter((_, index) => index <= 2);
-    const wetWeightedWeatherProportions = weightedWeatherProportions.filter((_, index) => index > 2);
-
     const getWeightedWeatherMultipliers = (proportions) => {
         const maxProportion = Math.max(...proportions);
         const minProportion = Math.min(...proportions);
@@ -52,7 +49,7 @@ const getWeightedWeatherTypeMultipliers = wetChance => {
         return proportions.map(proportion => (proportion - avgProportion) * multiplierWeight + 1);
     }
 
-    return [...getWeightedWeatherMultipliers(dryWeightedWeatherProportions), ...getWeightedWeatherMultipliers(wetWeightedWeatherProportions)]
+    return getWeightedWeatherMultipliers(weightedWeatherProportions)
 };
 
 // List of weather types
@@ -77,17 +74,38 @@ const weatherTypeDisplayMap = {
     veryWet: {label: "Very Wet", iconUrl: 'https://www.amcharts.com/wp-content/themes/amcharts4/css/img/icons/weather/animated/rainy-6.svg', discordEmoji: ':thunder_cloud_rain:'},
 }
 
-const stickyWeight = 1;
+const stickyWeight = 3.5;
+const expectedWeatherWeight = 1.5;
+
+const getExpectedWeatherIndex = (multipliers) => {
+    if (multipliers.length === 0) {
+        return -1;
+    }
+
+    let max = multipliers[0];
+    let maxIndex = 0;
+
+    for (var i = 1; i < multipliers.length; i++) {
+        if (multipliers[i] > max) {
+            maxIndex = i;
+            max = multipliers[i];
+        }
+    }
+
+    return maxIndex;
+}
 
 const getNextStintWeatherProbabilities = (currentStintWeatherType, wetChance) => {
     const weatherMultipliers = getWeightedWeatherTypeMultipliers(wetChance);
+    const expectedWeatherIndex = getExpectedWeatherIndex(weatherMultipliers);
 
     const nextStintWeatherProbabilities = weatherTypes.map((weatherType, index) => {
         const currentStintWeatherIndex = weatherTypes.indexOf(currentStintWeatherType);
         const differenceToCurrentWeather = Math.abs(currentStintWeatherIndex - index);
-        const weatherTypeProbability = (1 / ((differenceToCurrentWeather + 1) * stickyWeight)) * getWeatherTypeProbability(weatherType, wetChance);
+        const differenceToExpectedWeather = Math.abs(expectedWeatherIndex - index);
+        const weatherTypeProbability = ((1 / (Math.pow(differenceToCurrentWeather + 1, stickyWeight + 1) + Math.pow(differenceToExpectedWeather + 1, expectedWeatherWeight + 1))) * getWeatherTypeProbability(weatherType, wetChance));
 
-        return { type: weatherType, probability: weatherTypeProbability * weatherMultipliers[index] };
+        return { type: weatherType, probability: weatherTypeProbability };
     });
 
     return nextStintWeatherProbabilities;
